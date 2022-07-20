ae9d69b0bc8875e1cc4b4afd6cf811ce8aba7f9b
JuliaTanrikulu <109625953+JuliaTanrikulu@users.noreply.github.com>
2022-07-20 21:56:40 +0300

    Add files via upload

---
+++ Задание 1. Лекция 1.py
@@ -0,0 +1,58 @@
+Python 3.10.5 (tags/v3.10.5:f377153, Jun  6 2022, 16:14:13) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+my_str = input ("Hello")
+Hello
+a = 10
+b = 5
+print("Переменные a и b - " , a , b)
+Переменные a и b -  10 5
+string1 = input("Введите любое слово")
+Введите любое слово
+number1 = int(input("Введите число от 1 до 100К"
+                    1
+                    
+SyntaxError: '(' was never closed
+number1 = int(input("Введите число от 1 до 100К"
+                    1
+                    
+SyntaxError: '(' was never closed
+
+
+string1 = input("Введите любое слово")
+                    
+Введите любое слово
+
+
+
+фыв
+                    
+Traceback (most recent call last):
+  File "<pyshell#14>", line 1, in <module>
+    фыв
+NameError: name 'фыв' is not defined
+
+
+number1 = int(input("ВВедите первое число")
+              number = int(input("Введите второе число")
+                           
+SyntaxError: invalid syntax. Perhaps you forgot a comma?
+
+SyntaxError: invalid syntax. Perhaps you forgot a comma?number1 = int(input("Введите число от 1 до 100К"
+                    1
+                                                                            
+SyntaxError: invalid syntax
+SyntaxError: invalid syntax. Perhaps you forgot a comma?number1 = int(input("Введите число от 1 до 100К"
+                    1
+                                                                            
+
+
+
+
+
+
+
+
+
+
+
+
